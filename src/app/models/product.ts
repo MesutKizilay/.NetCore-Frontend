32424a92078f9b00c38cb7d81b34240f1d28261e
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-04-28 22:03:07 +0300

    product and productResponseModel were added.

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,7 +1,7 @@
 export interface Product{
-    productId:number
-    categoryId:number
-    productName:string
-    unitPrice:number
+    productId:number,
+    categoryId:number,
+    productName:string,
+    unitPrice:number,
     unitsInStock:number
 }
