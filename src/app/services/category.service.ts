c85829c68a21398d8ee090dd2ab5191c67ba9dc9
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-04-30 00:53:24 +0300

    Category service was added and categories were listed on website.

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Category } from '../models/category';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  apiUrl = 'https://localhost:44302/api/categories/getall';
+  
+  constructor(private httpClient: HttpClient) {}
+
+  getCategories(): Observable<ListResponseModel<Category>> {
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
+  }
+}
