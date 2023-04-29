7b6b0d268115e04e00d0162be8baf140fa2a716d
Mesut Kızılay <mesutkizilay3868@gmail.com>
2023-04-29 17:52:30 +0300

    Product component was modified

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
 })
-export class ProductComponent { 
-  products:Product[] = [];
+export class ProductComponent implements OnInit {
+  apiUrl = 'https://localhost:44302/api/products/getall';
+  products: Product[] = [];
+
+  productResponseModel:ProductResponseModel={
+    data:this.products,
+    message:"",
+    success:true
+  };
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products=response.data
+      });
+  }
 }
